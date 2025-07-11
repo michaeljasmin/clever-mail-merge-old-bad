a0eada63f5722e197497fe75a9e49968315f322e
Michael Jasmin <contact@michaeljasmin.com>
2025-07-11 05:37:43 -0400

    test

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to Clever Mail Merge.</h1>
+      <h1>Welcome to Clever Mail Merge!</h1>
     </main>
   );
 }
